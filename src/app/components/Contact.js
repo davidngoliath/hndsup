1146583c13f6e6620e188dec1b0e6fa1e7866460
rrudy90023 <rrudy90023@gmail.com>
2025-02-26 17:28:12 -0800

    submit states

--- src/app/components/Contact.js
+++ src/app/components/Contact.js
@@ -11,6 +11,7 @@ export default function Contact() {
   const [recipient, setRecipient] = useState('Your Local Police Department');
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(false);
+  const [showLoadingScreen, setShowLoadingScreen] = useState(false);
 
   const prewrittenLetter = `
     <p>Dear ${recipient},</p>
@@ -34,12 +35,14 @@ export default function Contact() {
     setMessage(prewrittenLetter);
   }, [name, recipient]);
 
-  const handleZipcodeChange = async (e) => {
-    const zipcode = e.target.value;
-    setZipcode(zipcode);
+  const handleSubmit = async (e) => {
+    // const zipcode = e.target.value;
+    // setZipcode(zipcode);
+    e.preventDefault();
 
     if (zipcode.length === 5) {
       setLoading(true);
+      setShowLoadingScreen(true);
       setRecipient('Locating...');
       try {
         const response = await fetch(`/api/getpolicedepartment?zipcode=${zipcode}`);
@@ -52,16 +55,20 @@ export default function Contact() {
         setError('Failed to fetch police department');
         console.error('Error fetching police department:', error);
       } finally {
+        // handleSubmit(e);
         setLoading(false);
+        setShowLoadingScreen(false);
+        // handleFormClear();
       }
     }
   };
 
-  const handleSubmit = (e) => {
+  const handleFormClear = (e) => {
     e.preventDefault();
     // Handle form submission logic here
     console.log('Form submitted:', { name, email, message, recipient });
     // Reset form fields
+    
     setName('');
     setEmail('');
     setZipcode('');
@@ -71,57 +78,61 @@ export default function Contact() {
 
   return (
     <div className={styles.contactContainer}>
-      
+      {showLoadingScreen && (
+        <div className={styles.loadingScreen}>
+          <h2>LOCATING LOCAL LAW ENFORCEMENT...</h2>
+        </div>
+      )}
       <form onSubmit={handleSubmit} className={styles.contactForm}>
-      <h1>WRITE A LETTER TO <span>your</span> LOCAL LAW ENFORCEMENT</h1>
-      <div className={styles.inputContainer}>
-        <div className={styles.userContainer}>
-          <div className={styles.formGroup}>
-            <label htmlFor="name">NAME</label>
-            <input
-              className={styles.input}
-              type="text"
-              id="name"
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-              required
-            />
+        <h1>WRITE A LETTER TO <span>your</span> LOCAL LAW ENFORCEMENT</h1>
+        <div className={styles.inputContainer}>
+          <div className={styles.userContainer}>
+            <div className={styles.formGroup}>
+              <label htmlFor="name">NAME</label>
+              <input
+                className={styles.input}
+                type="text"
+                id="name"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                required
+              />
+            </div>
+            <div className={styles.formGroup}>
+              <label htmlFor="zipcode">ZIPCODE</label>
+              <input
+                className={styles.input}
+                type="text"
+                id="zipcode"
+                value={zipcode}
+                onChange={(e) => setZipcode(e.target.value)}
+                required
+              />
+            </div>
+            <div className={styles.formGroup}>
+              <label htmlFor="email">EMAIL</label>
+              <input
+                className={styles.input}
+                type="email"
+                id="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                required
+              />
+            </div>
+            <button type="submit" className={styles.submitButton}>SUBMIT</button>
           </div>
-          <div className={styles.formGroup}>
-            <label htmlFor="zipcode">ZIPCODE</label>
-            <input
-              className={styles.input}
-              type="text"
-              id="zipcode"
-              value={zipcode}
-              onChange={handleZipcodeChange}
-              required
-            />
+          <div className={styles.messageContainer}>
+            <div className={styles.formGroup}>
+              <label htmlFor="message">SAMPLE LETTER</label>
+              <div
+                id="message"
+                className={styles.textarea}
+                dangerouslySetInnerHTML={{ __html: message }}
+              ></div>
+            </div>
           </div>
-          <div className={styles.formGroup}>
-            <label htmlFor="email">EMAIL</label>
-            <input
-              className={styles.input}
-              type="email"
-              id="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              required
-            />
-          </div>
-          <button type="submit" className={styles.submitButton}>SUBMIT</button>
         </div>
-        <div className={styles.messageContainer}>
-          <div className={styles.formGroup}>
-            <label htmlFor="message">SAMPLE LETTER</label>
-            <div
-              id="message"
-              className={styles.textarea}
-              dangerouslySetInnerHTML={{ __html: message }}
-            ></div>
-          </div>
-        </div>
-      </div>
       </form>
       
       {error && <p className={styles.error}>{error}</p>}
