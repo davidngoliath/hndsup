2439eb9f5cb3114054e97dd0e7ae66ac809fb03e
rrudy90023 <rrudy90023@gmail.com>
2025-03-31 15:29:51 -0700

    share image

--- src/app/layout.js
+++ src/app/layout.js
@@ -65,7 +65,7 @@ export default function RootLayout({ children }) {
         <meta property="og:type" content="website"/>
         <meta property="og:title" content="Hndsup | Emergency Alert Wearable for Police Stops"/>
         <meta property="og:description" content="Hndsup is the wearable that doesn’t tell time—it protects lives. Designed to keep you safe during police encounters. Learn more and support the mission."/>
-        <meta property="og:image" content="https://dng-com.s3.amazonaws.com/clients/hndsup/images/key-image-hnds-up.jpg"/>
+        <meta property="og:image" content="shareimage.jpg"/>
 
 
         <meta name="twitter:card" content="summary_large_image"/>
@@ -73,7 +73,7 @@ export default function RootLayout({ children }) {
         <meta property="twitter:url" content="https://hndsup.com/"/>
         <meta name="twitter:title" content="Hndsup | The Product We Hope To Never Launch"/>
         <meta name="twitter:description" content="Wearable emergency alert device to promote safety during police stops."/>
-        <meta name="twitter:image" content="https://dng-com.s3.amazonaws.com/clients/hndsup/images/key-image-hnds-up.jpg"/>
+        <meta name="twitter:image" content="shareimage.jpg"/>
 
 
 
