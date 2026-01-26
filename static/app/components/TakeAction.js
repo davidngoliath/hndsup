2afdfb9672f4a66674c2e75983fb931e82aef0f5
rrudy90023 <rrudy90023@gmail.com>
2026-01-26 14:58:44 -0800

    results section 1

--- static/app/components/TakeAction.js
+++ static/app/components/TakeAction.js
@@ -36,6 +36,8 @@ export default function TakeAction({actionRef}) {
                         </p>
                         <p>
                             We asked people to support Courageous Conversation, an organization offering racial equity and de-escalation training. Their programs give officers the tools to build trust and handle difficult situations with care.
+                        </p>
+                        <p>
                             Because while technology can record what happens, only education and conversation can prevent it from happening in the first place.
                         </p>
                         {/* <video src={getAssetPath('/images/takeaction-bg.mp4')} width="1544" height="876" loop autoPlay playsInline muted 
